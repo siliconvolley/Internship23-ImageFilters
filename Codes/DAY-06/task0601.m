11884eb1a38b1df13154e5a381313681a86b5412
siliconvolley <rishabhlaxmikanth@gmail.com>
2023-10-26 23:27:44 +0530

    added DAY-06 codes

--- Codes/DAY-06/task0601.m
+++ Codes/DAY-06/task0601.m
@@ -1,23 +1,24 @@
 % 20 images to BW and save in CSV
 pathList = [];
-imagesFinalMatrix = zeros(20, (225*225));
+imagesFinalMatrix = zeros(6, (128*128));
 
 SOURCE_FOLDER_PATH = 'sample_photos/';
 RESIZED_FOLDER_PATH = 'images_resized/';
 GRAY_FOLDER_PATH = 'images_gray/';
 BW_FOLDER_PATH = 'images_BW/';
-THRESHOLD = 120;
-FORMAT = '.jpg';
+THRESHOLD = 110;
+FORMAT = '.png';
+IMAGE_COUNT = 6;
 
-for imageIndex = 1:20
+for imageIndex = 1:IMAGE_COUNT
     % saving image paths in a list
     imageName = string(imageIndex);
     sourcePath = strcat(SOURCE_FOLDER_PATH,imageName,FORMAT);
     pathList = [pathList, sourcePath];
 
-    % resizing the images to 225x225
+    % resizing the images to 256x256
     img = imread(sourcePath);
-    imgResized = imresize(img, [225 225]);
+    imgResized = imresize(img, [256 256]);
     resizedPath = strcat(RESIZED_FOLDER_PATH,imageName,FORMAT);
     imwrite(imgResized, resizedPath);
 
@@ -57,11 +58,37 @@ for imageIndex = 1:20
 
     imwrite(blackwhiteChannel, bwPath);
 
+    % Sub-sampling
+    subSampledMatrix = [];
+
+    for outerMatrixRow = 1:2
+    	for outerMatrixColumn = 1:2
+	    	innerRowStart = (outerMatrixRow - 1) * 128 + 1;
+		    innerRowEnd = outerMatrixRow * 128;
+		    innerColumnStart = (outerMatrixColumn - 1) * 128 + 1;
+		    innerColumnEnd = outerMatrixColumn * 128;
+
+            bwImageRange = blackwhiteChannel(innerRowStart:innerRowEnd, innerColumnStart:innerColumnEnd);
+        
+		    subSampledMatrix(:, :, imageIndex) = bwImageRange;
+        end
+    end	
+
     % converting BW matrix to vector
-    bwVector = blackwhiteChannel';
+    bwVector = subSampledMatrix(:, :, imageIndex)';
     bwVector = bwVector(:)';
 
+    % horizontal and vertical profiling
+    row = subSampledMatrix(1, :, imageIndex);
+    column = subSampledMatrix(:, 1, imageIndex);
+
+    % counting 1(s) in the vector
+    oneRowSum = sum(row(:) == 1);
+    oneColumnSum = sum(column(:) == 1);
+    oneRowSum
+    oneColumnSum
+
     % adding the binary image to a matrix
-    imagesFinalMatrix(imageIndex,:) = bwVector;
-    writematrix(imagesFinalMatrix, 'matrix.csv')
+    imagesFinalMatrix(imageIndex, :) = bwVector;
 end
+writematrix(imagesFinalMatrix, 'Final Sub-Sampled Matrix.csv');
